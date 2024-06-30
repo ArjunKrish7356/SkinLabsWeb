ed72e70798934eaa4ca1b09422400aea32916c13
M Arjun Krishna <119443675+ArjunKrish7356@users.noreply.github.com>
2024-06-30 17:00:10 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,149 @@
-import Image from "next/image";
+import React from 'react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+ <main className=' block'>
+  <div className=" h-auto w-auto">
+    <div className=" top h-screen relative w-full flex overflow-hidden">
+      <img className=" absolute left-0 h-auto md:h-auto m-5 md:m-10 z-10 size-40 md:size-60  top-0 " src="https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f20ebeb69a12b8e50d2653b_skinlabs_logo.svg" alt="Logo"></img>
+      <div className=" bg-center bg-cover bg-[url('https://images.pexels.com/photos/3762875/pexels-photo-3762875.jpeg?cs=srgb&dl=pexels-shiny-diamond-3762875.jpg&fm=jpg')] h-screen w-full md:w-1/2 arjun z-0 relative"></div>
+      <div className=" bg-center bg-cover bg-[url('https://images.pexels.com/photos/3762466/pexels-photo-3762466.jpeg')]  hidden md:block h-screen w-1/2 z-0 relative"></div>
+      <h1 className="absolute font-serif left-0 m-5 md:m-10 md:mb-24 text-black text-xl md:text-3xl top-1/2 z-10" >Reflection in Modernity</h1>
+      <h1 className="absolute font-serif font-normal left-0 m-5 md:m-10 mt-14 md:mt-20 text-black text-7xl md:text-9xl top-1/2 w-auto md:w-2/6 z-10">The Skincare Revolution</h1>
+   </div>
+   <div className="product h-screen w-full ">
+      <h1 className=" font-serif font-medium lg:font-thin mt-20 lg:mt-60 mx-5 lg:mx-10 mb-12 lg:mb-28 text-4xl lg:text-8xl text-black">Featured products</h1>
+
+      <div className="product-navbar h-min">
+
+          <div className='navbar sticky top-0 z-30 -translate-x-10 text-black text-2xl w-screen bg-slate-800 h-0 text-right float-end'>
+            <div className=' bg-white w-1/2 h-0'></div>
+            <div className=" overflow-hidden translate-x-5 bg-slate-100 mx-5 lg:flex h-24 lg:h-36 lg:justify-evenly text-lg lg:text-3xl text-gray-500 w-full lg:w-1/2 lg:float-end">
+                  <h1 className=" my-1 text-center  lg:flex-col lg:content-around lg:w-1/4">Filter by category</h1>
+                  <div className=" flex justify-evenly my-2 w-auto lg:w-3/4">
+                    <a href="#clean" className=" border-solid border-2 border-slate-100 lg:flex-col lg:content-around  target:border-black px-8 py-2 ">Clean</a>
+                    <a href="#treat" className=" border-solid border-2 border-slate-100 lg:flex-col lg:content-around  target:border-black px-8 py-2">Treat</a>
+                    <a href="#moist" className=" border-solid border-2 border-slate-100 lg:flex-col lg:content-around  target:border-black px-8 py-2">Moisturise</a>
+                  </div>
+            </div>
+          </div>
+            <div className= ' lg:hidden h-36 w-full '></div>
+            <div id="clean" className=" h-[50vh] md:h-screen ">
+              <h1 className=" bg-white  font-serif font-thin hidden lg:flex items-center mx-10 my-20 h-36 align-middle text-6xl text-black top-0 sticky">Best selling cleansers</h1>
+              <div className='snap-x no-scrollbar overflow-x-scroll h-full w-auto flex '>
+                <div className=' snap-center h-3/4 min-w-60'>
+                    <div className=' photo'>
+                      <img src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f114a1c26b4cf1bdc88fe2a_Glass-Bottle-MockupTransparent.png'></img>
+                    </div>
+                    <div className=' text h-1/4 w-full'>
+                      <h1 className=' text-black text-xs'>PROTECTS AND MOISTURIZES</h1>
+                      <h1 className=' text-black text-xl font-bold '>Oat Cleansing Balm</h1>
+                    </div>               
+                </div>
+                <div className=' snap-center h-3/4 min-w-60'>
+                    <div className=' photo'>
+                    <img src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f114a1c26b4cf1bdc88fe2a_Glass-Bottle-MockupTransparent.png'></img>
+                    </div>
+                    <div className=' text h-1/4 w-full'>
+                      <h1 className=' text-black text-xs'>PROTECTS AND MOISTURIZES</h1>
+                      <h1 className=' text-black text-xl font-bold '>Oat Cleansing Balm</h1>
+                    </div>     
+                </div>
+                <div className=' snap-center h-3/4 min-w-60'>
+                  <div className=' photo'>
+                  <img src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f114a1c26b4cf1bdc88fe2a_Glass-Bottle-MockupTransparent.png'></img>
+                  </div>
+                  <div className=' text h-1/4 w-full'>
+                      <h1 className=' text-black text-xs'>PROTECTS AND MOISTURIZES</h1>
+                      <h1 className=' text-black text-xl font-bold '>Oat Cleansing Balm</h1>
+                    </div>     
+                </div>
+              </div>
+            </div>
+            <div id="treat" className=" h-[50vh] md:h-screen ">
+              <h1 className=" bg-white  font-serif font-thin hidden lg:flex items-center mx-10 my-20 h-36 align-middle text-6xl text-black top-0 sticky">Treatment solutions</h1>
+              <div className='snap-x no-scrollbar overflow-x-scroll h-full w-auto flex '>
+                <div className=' snap-center h-3/4 min-w-60 '>
+                    <div className=' photo'>
+                    <img src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f114a1c26b4cf1bdc88fe2a_Glass-Bottle-MockupTransparent.png'></img>
+                    </div>
+                    <div className=' text h-1/4 w-full'>
+                      <h1 className=' text-black text-xs'>PROTECTS AND MOISTURIZES</h1>
+                      <h1 className=' text-black text-xl font-bold '>Oat Cleansing Balm</h1>
+                    </div>                
+                </div>
+                <div className=' snap-center h-3/4 min-w-60 '>
+                  <div className=' photo'>
+                    <img src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f114a1c26b4cf1bdc88fe2a_Glass-Bottle-MockupTransparent.png'></img>
+                  </div>
+                  <div className=' text h-1/4 w-full'>
+                      <h1 className=' text-black text-xs'>PROTECTS AND MOISTURIZES</h1>
+                      <h1 className=' text-black text-xl font-bold '>Oat Cleansing Balm</h1>
+                    </div>     
+                </div>
+                <div className=' snap-center h-3/4 min-w-60 '>
+                  <div className=' photo'>
+                  <img src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f114a1c26b4cf1bdc88fe2a_Glass-Bottle-MockupTransparent.png'></img>
+                  </div>
+                  <div className=' text h-1/4 w-full'>
+                      <h1 className=' text-black text-xs'>PROTECTS AND MOISTURIZES</h1>
+                      <h1 className=' text-black text-xl font-bold '>Oat Cleansing Balm</h1>
+                    </div>      
+                </div>
+              </div>
+            </div>
+            <div id="moist" className=" h-[50vh] md:h-screen">
+              <h1 className=" bg-white  font-serif font-thin hidden lg:flex items-center mx-10 my-20 h-36 align-middle text-6xl text-black top-0 sticky">For moisturised skin</h1>
+              <div className='snap-x no-scrollbar overflow-x-scroll h-full w-auto md:w-full flex   '>
+                <div className=' snap-center h-3/4 min-w-60'>
+                    <div className=' photo '>
+                      <img className='img-photo' src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f114a1c26b4cf1bdc88fe2a_Glass-Bottle-MockupTransparent.png'></img>
+                    </div>
+                    <div className=' text h-1/4 w-full'>
+                      <h1 className=' text-black text-xs'>PROTECTS AND MOISTURIZES</h1>
+                      <h1 className=' text-black text-xl font-bold '>Oat Cleansing Balm</h1>
+                    </div>              
+                </div>
+                <div className=' snap-center h-3/4 min-w-60 '>
+                    <div className=' photo'>
+                    <img src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f114a1c26b4cf1bdc88fe2a_Glass-Bottle-MockupTransparent.png'></img>
+                    </div>
+                    <div className=' text h-1/4 w-full'>
+                      <h1 className=' text-black text-xs'>PROTECTS AND MOISTURIZES</h1>
+                      <h1 className=' text-black text-xl font-bold '>Oat Cleansing Balm</h1>
+                    </div>     
+                </div>
+                <div className=' snap-center h-3/4 min-w-60 '>
+                    <div className=' photo'>
+                    <img src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f114a1c26b4cf1bdc88fe2a_Glass-Bottle-MockupTransparent.png'></img>
+                    </div>
+                    <div className=' text h-1/4 w-full'>
+                      <h1 className=' text-black text-xs'>PROTECTS AND MOISTURIZES</h1>
+                      <h1 className=' text-black text-xl font-bold '>Oat Cleansing Balm</h1>
+                    </div>    
+                </div>
+              </div>
+            </div>
+      
+      </div> 
+   </div>
+  </div>
+   <div className='contact h-[85vh]  w-full  mt-[55vh] md:mt-[270vh] block md:flex'>
+    <div className='contact-text font-serif px-5 md:px-10 h-1/2 md:h-full md:w-1/2 pt-[50%] md:pt-0 md:content-center'>
+      <div className=' md:w-fit md:h-fit ' >
+        <h1 className='w-fit text-3xl md:text-9xl font-medium  text-black  '>We're Social</h1>
+        <p className=' text-lg md:text-3xl pt-2 text-gray-500 md:leading-loose'>Join our community of skincare junkies like you who know that a great skicare routine goes a long way.</p>
+        <button className=' bg-black text-white min-h-16 w-full md:w-96 md:font-medium md:text-3xl mt-5 md:min-h-24 '>FIND US ON SOCIAL</button>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+    <div className=' grid grid-cols-2 grid-rows-3 contact-img  h-1/2 md:h-full w-full md:w-1/2 px-5 md:px-10 text-black pt-20 md:pt-0'>
+       <img loading='lazy' src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f20ea5ac3de55e1de2b1674_img1.jpg'></img>
+       <img loading='lazy' src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f20ea7c2711ae005accd97e_img3.jpg'></img>
+       <img loading='lazy' src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f20ea8c0b51f19c2eeae6ea_img4.jpg'></img>
+       <img loading='lazy' src='https://assets.website-files.com/5f040c8ad0b82a728ec61a1e/5f20ea6f2711ae9f30ccd97d_img.jpg'></img>
+    </div>
+   </div>
+  </main>
+  
   );
 }
